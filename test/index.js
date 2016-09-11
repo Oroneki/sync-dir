4a291f145ea932f1ddc784d9522fd73da9ffa4e2
frontainer <frontainer@gmail.com>
2016-09-11 17:58:36 +0900

    テストコードを微修正

--- test/index.js
+++ test/index.js
@@ -5,16 +5,18 @@ const syncDir = require('../index');
 const rimraf = require('rimraf');
 
 describe('sync-dir', function() {
+    const DEST = './dest';
     const SYNC_DATA = {
         sync: {
-            "./test/files/*": "./test/files/dest"
+            "./test/files/*.txt": DEST
         }
     };
+
     beforeEach(() => {
-        rimraf.sync('./test/files/dest');
+        rimraf.sync(DEST);
     });
     afterEach(() => {
-        rimraf.sync('./test/files/dest');
+        rimraf.sync(DEST);
     });
     it('getFiles',() => {
         let files = syncDir.getFiles(SYNC_DATA);
@@ -24,21 +26,21 @@ describe('sync-dir', function() {
     });
     it('copy',(done) => {
         syncDir.copy(SYNC_DATA).then((res) => {
-            assert(fs.existsSync('./test/files/dest/dummy.txt'));
-            assert(fs.existsSync('./test/files/dest/dummy2.txt'));
+            assert(fs.existsSync(`${DEST}/dummy.txt`));
+            assert(fs.existsSync(`${DEST}/dummy2.txt`));
             done();
         });
     });
     it('core - dry run',() => {
-        let files = syncDir(SYNC_DATA,true).files;
+        let files = syncDir(SYNC_DATA).files;
         assert(files.length === 2);
         assert(files[0] === './test/files/dummy.txt');
         assert(files[1] === './test/files/dummy2.txt');
     });
     it('core',(done) => {
-        syncDir(SYNC_DATA,false).promise.then((res) => {
-            assert(fs.existsSync('./test/files/dest/dummy.txt'));
-            assert(fs.existsSync('./test/files/dest/dummy2.txt'));
+        syncDir(SYNC_DATA).promise.then((res) => {
+            assert(fs.existsSync(`${DEST}/dummy.txt`));
+            assert(fs.existsSync(`${DEST}/dummy2.txt`));
             done();
         });
     });
